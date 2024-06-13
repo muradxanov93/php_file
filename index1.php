0ba520c5ca8990cb25291bf8779de6e40882ad94
muradxanov93 <zahidmuradxanov@gmail.com>
2024-06-13 15:31:32 +0400

    first commit

---
+++ index1.php
@@ -0,0 +1,116 @@
+<?php
+//echo "Salam";
+//define("MAY", "bu gun ayin 13dir" );
+//echo MAY;
+//
+//define("PI",3.14);
+//
+//function calculateCircleArea ($x)
+//{
+//    return $x*PI*$x;
+//}
+//
+//$radius = 5;
+//$area = calculateCircleArea($radius);
+//echo "The area of a circle with radius $radius is $area.";
+
+
+//$b=11;
+//
+//if ($b>0){
+//    echo "pozitiv";
+//}
+//elseif ($b<0){
+//    echo "neqativ";
+//}
+//else{
+//    "zero";
+//}
+//
+
+//function endirimiHesabla ($uzuvluk,$mebleg)
+//{
+//    $endirim=0;
+//    if ($uzuvluk=="gold"){
+//        if ($mebleg>100){
+//           $endirim= $mebleg*0.2;
+//        }
+//        else{
+//           $endirim= $mebleg*0.15;
+//        }
+//    }
+//    elseif ($uzuvluk=="silver"){
+//        if ($mebleg>100){
+//           $endirim= $mebleg*0.2;
+//        }
+//        else{
+//            $endirim=$mebleg*0.15;
+//        }
+//    }
+//    return $mebleg-$endirim;
+//}
+//
+//echo endirimiHesabla("gold",50);
+
+
+
+//for ($i=0; $i<=10; $i++){
+//    echo ($i);
+//}
+//
+//for($a=10; $a>=0;$a--){
+//    echo ($a);
+//}
+
+
+//function ss ($x,$y)
+//{
+//    return $y + $x;
+//
+
+//function ssssr($x,$y,$op)
+//{
+//    switch  ($op){
+//        case '*';
+//            return $x*$y;
+//        case '/';
+//            return $x/$y;
+//    }
+//
+//};
+//echo ssssr(15,100,"/");
+//
+//function df($a,$b,$per)
+//{
+//    switch ($per){
+//        case '-';
+//            return $a-$b;
+//        case '+';
+//            return $a-$b;
+//    }
+//};
+//echo df(54,54,'-');
+
+
+//function son ($number)
+//{
+//
+//    while ($number<10){
+//    echo $number/10;
+//    $number++;
+//
+//}
+//}
+//echo son(123);
+
+
+
+
+
+
+
+
+
+
+
+
